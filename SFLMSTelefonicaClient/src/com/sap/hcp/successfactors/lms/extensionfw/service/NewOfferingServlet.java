c1b0a5410470d3090cde14522dd8be9462b29f59
I322850 <I322850@INLN50905583A.apj.global.corp.sap>
2016-02-12 14:54:58 +0530

    new

--- SFLMSTelefonicaClient/src/com/sap/hcp/successfactors/lms/extensionfw/service/NewOfferingServlet.java
+++ SFLMSTelefonicaClient/src/com/sap/hcp/successfactors/lms/extensionfw/service/NewOfferingServlet.java
@@ -195,7 +195,7 @@ public class NewOfferingServlet {
 				days, true);
 		logger.error("M1 is back after datalist"+ "  "+dataList.size());
 		for(Offering offer :dataList){
-			logger.error("D1"+offer.getOfferingId());
+			logger.error("D1"+offer.getOfferingId()); 
 		}
 		Map<String, String> mp = getItemOfferingListData();
 		 logger.error("M2 is back after itemData"+ "  "+mp.size());
